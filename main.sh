a237d08f32aefa6fb799defae83ae357afd926fc
code1cats <codecats@devs4mex.com>
2023-03-31 01:55:05 -0600

    fix optimization

--- main.sh
+++ main.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-temp=""
-
-while read -r path; do
-    temp+="'"
-    temp+="${path}"
-    temp+="',"
-done < <(git diff --name-only -r HEAD^1 HEAD | grep env | cut -d/ -f1 | uniq)
-
-temp=$(echo $temp | sed -e '$s/,$//' )
+temp="$(git diff --name-only -r HEAD^1 HEAD | grep env | cut -d/ -f1 | uniq | paste -sd "," -)"
 temp="{'name': [$temp]}"
 echo $temp
 
+paths=$(git diff --name-only -r HEAD^1 HEAD | grep env | cut -d/ -f1 | sort -u | jq -R -s 'split("\n")[:-1] | map({"name": .})')
 
 echo  paths="[$(git diff --name-only -r HEAD^1 HEAD | grep env | cut -d/ -f1 | uniq | tr '\n' , | sed -e '$s/,$//')]"
