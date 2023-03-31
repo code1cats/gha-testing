3ad3465e439474f78910a1b3677096f6e3078ac2
code1cats <codecats@devs4mex.com>
2023-03-30 23:46:49 -0600

    test matrix

--- main.sh
+++ main.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-var=()
-
-while read -r path; do
-    var+="${path}"
-done < <(git diff --name-only -r HEAD^1 HEAD | grep env | cut -d/ -f1 | uniq)
-
-echo $var
+echo  "test=[$(git diff --name-only -r HEAD^1 HEAD | grep env | cut -d/ -f1 | uniq | tr '\n' , | sed -e '$s/,$//')]"
