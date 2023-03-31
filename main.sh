b8d26aaac20d88a5c33f69be5545df2ea4b9e6a1
code1cats <codecats@devs4mex.com>
2023-03-31 00:23:38 -0600

    Test restore

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo  "test=[$(git diff --name-only -r HEAD^1 HEAD | grep env | cut -d/ -f1 | uniq | tr '\n' , | sed -e '$s/,$//')]"
+echo  "test={$(git diff --name-only -r HEAD^1 HEAD | grep env | cut -d/ -f1 | uniq | tr '\n' , | sed -e '$s/,$//')}" 
