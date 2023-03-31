5ec819acb0ddc88aa6734ef9b472fc0fc40d69c5
code1cats <codecats@devs4mex.com>
2023-03-31 01:08:33 -0600

    mod files for testing

--- main.sh
+++ main.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-temp1=$(git diff --name-only -r HEAD^1 HEAD | grep env | cut -d/ -f1 | uniq | tr '\n' , | sed -e '$s/,$//')
-temp2="{'name':[$temp1]}"
-echo  "paths=$temp2"
+
+temp1=""
+
+while read -r path; do
+    temp1+="'"
+    temp1+="${path}"
+    temp1+="'"
+done < <(git diff --name-only -r HEAD^1 HEAD | grep env | cut -d/ -f1 | uniq)
+
+echo $temp1
+
+
+echo  "paths=$(git diff --name-only -r HEAD^1 HEAD | grep env | cut -d/ -f1 | uniq | tr '\n' , | sed -e '$s/,$//')"
