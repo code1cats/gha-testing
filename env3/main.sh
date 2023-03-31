47ca2ce2557896c136bcc583a69f417e0169561a
code1cats <codecats@devs4mex.com>
2023-03-30 21:41:33 -0600

    add script for every environment

---
+++ env3/main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "Hello, World! for env3"
+  else
+    echo "Hello, $1!"
+fi
+
---
+++ env4/main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "Hello, World! from env4"
+  else
+    echo "Hello, $1!"
+fi
+
