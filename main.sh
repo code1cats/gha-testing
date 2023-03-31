0eb5ab02dab58c63fd4b3406005eba82de00e0ad
code1cats <codecats@devs4mex.com>
2023-03-31 00:57:58 -0600

    temp 11

--- main.sh
+++ main.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-echo  "paths=$(git diff --name-only -r HEAD^1 HEAD | grep env | cut -d/ -f1 | uniq | tr '\n' , | sed -e '$s/,$//')"
+temp1=$(git diff --name-only -r HEAD^1 HEAD | grep env | cut -d/ -f1 | uniq | tr '\n' , | sed -e '$s/,$//')
+temp2="{'name':[$temp1]}"
+echo  "paths=$temp2"
