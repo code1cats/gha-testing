386efbc6cdb4a8d711d1884a6cecfc905bd4214c
code1cats <codecats@devs4mex.com>
2023-03-31 01:16:18 -0600

    prueba 12

--- main.sh
+++ main.sh
@@ -5,10 +5,12 @@ temp1=""
 while read -r path; do
     temp1+="'"
     temp1+="${path}"
-    temp1+="'"
+    temp1+="',"
 done < <(git diff --name-only -r HEAD^1 HEAD | grep env | cut -d/ -f1 | uniq)
 
-echo $temp1
+temp2=$(echo $temp1 | sed -e '$s/,$//' )
+
+echo $temp2
 
 
 echo  "paths=$(git diff --name-only -r HEAD^1 HEAD | grep env | cut -d/ -f1 | uniq | tr '\n' , | sed -e '$s/,$//')"
